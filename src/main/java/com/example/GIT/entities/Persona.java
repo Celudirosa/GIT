850016b5ba3bccfe7c3b575497bad02e3e436eb4
celudirosa <cluqued@live.com>
2024-02-14 13:01:34 +0100

    add entitie persona

---
+++ src/main/java/com/example/GIT/entities/Persona.java
@@ -0,0 +1,33 @@
+package com.example.GIT.entities;
+
+import java.io.Serializable;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "estudiantes")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Persona implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String nombre;
+    private String primerApellido;
+    private String segundoApellido;
+    
+}
